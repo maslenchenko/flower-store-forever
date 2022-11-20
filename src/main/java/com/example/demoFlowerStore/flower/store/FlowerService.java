e710d02aa833586a03f59fbef9a0b14ed374bcd8
oleksandra <91615687+maslenchenko@users.noreply.github.com>
2022-11-21 00:40:48 +0200

    Update FlowerService.java

--- src/main/java/com/example/demoFlowerStore/flower/store/FlowerService.java
+++ src/main/java/com/example/demoFlowerStore/flower/store/FlowerService.java
@@ -16,12 +16,6 @@ public class FlowerService {
     }
 
     public List<Flower> getFlowers(){
-//        List<Flower> objects = Arrays.asList(
-//                new Flower(10.0, RED, 40.0, FlowerType.TULIP, 100L),
-//                new Flower(10.0, WHITE, 20.0, FlowerType.CHAMOMILE, 10L),
-//                new Flower(5.0, YELLOW, 50.0, FlowerType.ROSE, 1L)
-//        );
-//        return objects;
         return flowerRepository.findAll();
     }
 
